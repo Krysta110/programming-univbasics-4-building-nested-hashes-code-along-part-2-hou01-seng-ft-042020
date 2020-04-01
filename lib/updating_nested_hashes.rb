6369ca53098003042a70893088fb9abbbd55a09c
Krysta Wolkowiecki <krystawolkowiecki@outlook.com>
2020-04-01 07:24:47 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -63,6 +63,10 @@ def add_characters
 epic_tragedy[:additional_characters][0][:name] = "Prince Escalus"
 epic_tragedy[:additional_characters][1][:name] = "Apothecary"
 
+add = {
+  :name => "Prince Escalus"
+  :name => "Apothecary"
+} 
 
 
 
